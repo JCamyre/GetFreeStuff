fb79a0f1400df810840dedfb4dda4998d0bc5fe4
JCamyre <JWcamry03@gmail.com>
2021-08-23 20:27:53 -0700

    Fix accessing environment variable

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['GETFREESTUFF_SECRET_KEY']
+SECRET_KEY = os.getenv('GETFREESTUFF_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework', 
+    'corsheaders',
+    'students'
 ]
 
 MIDDLEWARE = [
@@ -48,7 +51,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.CommonMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 ROOT_URLCONF = 'mysite.urls'
 
